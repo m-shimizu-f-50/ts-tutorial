c547493d50707ed2851540f1b115d9b9a368462e
m-shimizu <62657297+m-shimizu-ring@users.noreply.github.com>
2022-02-26 00:11:49 +0900

    返却値の型指定

--- src/practices/Practice1.tsx
+++ src/practices/Practice1.tsx
@@ -6,7 +6,7 @@ export const Practice1 = () => {
   const onClickPractice = () => calcTotalFee(1000);
   return (
     <div>
-      <p>練習問題：引数の型指定</p>
+      <p>練習問題１：引数の型指定</p>
       <button onClick={onClickPractice}>練習問題を1を実行</button>
     </div>
   );
